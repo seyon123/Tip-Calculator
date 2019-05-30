cc37e41709dc430a0be0170b9a471027930c3b5e
Seyon Rajagopal <r_seyon@live.ca>
2019-05-29 23:34:13 -0400

    Added other percentage functionand bug fixes

--- js/index.js
+++ js/index.js
@@ -4,6 +4,10 @@ function calculateTip() {
   var tipAmount = document.getElementById("tipPercentage").value;
   var numOfPeople = document.getElementById("numPeople").value;
 
+  if (tipAmount === "other") {
+    tipAmount = document.getElementById("tipNum").value/100;
+  }
+
   //validate input
   if (billAmt === "" || tipAmount == 0) {
     alert("Please make sure ALL values are entered");
@@ -16,8 +20,7 @@ function calculateTip() {
   } else {
     document.getElementById("each").style.display = "block";
   }
- 
-  
+
   //Calculate tip
   var total = (billAmt * tipAmount) / numOfPeople;
   //round to two decimal places
@@ -32,22 +35,40 @@ function calculateTip() {
 //Clear the form
 function clearForm() {
   document.getElementById("form").reset();
+  document.getElementById("numPeople").value = "";
 }
 
+//function that shows or hides the "other" textbox
+function show(value) {
+  if (value == "other") {
+    tipNum.style.display='inline-block';
+    percentage.style.display='inline-block';
+  } 
+  else{
+    tipNum.style.display='none';
+    percentage.style.display='none';
+  }
+}
 //Hide the tip amount on load
 document.getElementById("totalTip").style.display = "none";
 document.getElementById("each").style.display = "none";
+document.getElementById("tipNum").style.display = "none";
+document.getElementById("percentage").style.display = "none";
+//document.getElementById("text").style.display = "none";
 
 //click calculate button to call calculate function
 document.getElementById("calculate").onclick = function() {
   calculateTip();
 
-
 };
+
 //click clear button to call clear function
 document.getElementById("clear").onclick = function() {
   clearForm();
   document.getElementById("totalTip").style.display = "none";
-  document.getElementById("each").style.display = "none"; 
+  document.getElementById("each").style.display = "none";
+  document.getElementById("tipNum").style.display = "none";
+  document.getElementById("percentage").style.display = "none";
 
 };
+
